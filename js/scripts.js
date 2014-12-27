484ece98e41a12325b5c9ee3f92f88860edeb883
JGiammona <Valhalla@James-MacBook-Pro.local>
2014-12-26 17:18:43 -0800

    Added Website files

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+  $('.animated').each(function (index, element) {
+    $(element).addClass('animation-active');
+  })
+};
+
+
+// Smooth scrolling
+// http://css-tricks.com/snippets/jquery/smooth-scrolling/
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1200);
+        return false;
+      }
+    }
+  });
+});
